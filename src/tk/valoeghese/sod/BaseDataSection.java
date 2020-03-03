c4f13828d261970db55b974e373bd726d0f50eeb
valoeghese <valoeghesemods@gmail.com>
2020-03-03 15:07:14 +1300

    2.0.1 - make BaseDataSection public

--- src/tk/valoeghese/sod/BaseDataSection.java
+++ src/tk/valoeghese/sod/BaseDataSection.java
@@ -1,6 +1,6 @@
 package tk.valoeghese.sod;
 
-interface BaseDataSection<E> extends Iterable<E> {
+public interface BaseDataSection<E> extends Iterable<E> {
 	/**
 	 * @deprecated Should only be used by the parser! Please use the type specific methods instead for writing data.
 	 */
